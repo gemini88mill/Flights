0dd4760071bb3c8a9f1b372a0f757e1ee4994b71
Raphael <gemini88mill@gmail.com>
2015-05-28 12:06:04 -0400

    added window functionality(WIP)

--- src/main/java/net/raphaelmiller/LanternaHandler.java
+++ src/main/java/net/raphaelmiller/LanternaHandler.java
@@ -1,19 +1,7 @@
 package net.raphaelmiller;
 
 import com.googlecode.lanterna.TerminalFacade;
-import com.googlecode.lanterna.gui.Action;
-import com.googlecode.lanterna.gui.Border;
 import com.googlecode.lanterna.gui.GUIScreen;
-import com.googlecode.lanterna.gui.Window;
-import com.googlecode.lanterna.gui.component.Button;
-import com.googlecode.lanterna.gui.component.Label;
-import com.googlecode.lanterna.gui.component.Panel;
-import com.googlecode.lanterna.gui.layout.LayoutParameter;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.TerminalSize;
-
-import java.nio.charset.Charset;
 
 /**
  * Created by raphael on 5/26/15.
@@ -23,7 +11,7 @@ public class LanternaHandler  {
 
     public void LanternaTerminal(FlightsClient flc){
         //creates lanterna terminal window
-        TerminalWindow tw = new TerminalWindow("QPX", flc);
+        GUIWindow tw = new GUIWindow("QPX", flc);
 
         GUIScreen guiScreen = TerminalFacade.createGUIScreen();
         guiScreen.getScreen().startScreen();
