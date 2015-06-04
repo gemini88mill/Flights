fc58184c7d596fe075c6cd6276978421c40d8af2
Raphael <gemini88mill@gmail.com>
2015-06-03 22:39:22 -0400

    removed unused methods

--- src/main/java/net/raphaelmiller/GUIWindow.java
+++ src/main/java/net/raphaelmiller/GUIWindow.java
@@ -224,86 +224,8 @@ public class GUIWindow extends Window {
         //results.appendLine(textValues);
     }
 
-    //--------------------methods not in use------------------------------------------
-    //--------------------------------------------------------------------------------
+    
 
-
-    //attempts at second page, left in for reference ----------------------------------
-    private void lanternaLogin() {
-        final TextBox username = new TextBox("Username", 400);
-        final PasswordBox password = new PasswordBox(null, 400);
-
-        addComponent(new Label("", Terminal.Color.BLACK));
-        addComponent(username);
-        addComponent(new Label("Password:", Terminal.Color.BLACK));
-        addComponent(password);
-
-        addComponent(new Button("ENTER", new Action() {
-            @Override
-            public void doAction() {
-                uiUser = username.getText();
-                uiPassword = password.getText();
-
-                System.out.println(uiUser + " " + uiPassword);
-
-                //lanternaStartMenu();
-            }
-        }));
-        addComponent(new Button("Quit", new Action() {
-            @Override
-            public void doAction() {
-                System.exit(0);
-            }
-        }));
-    }
-
-    private void lanternaStartMenu(FlightsClient flc) {
-
-        final TextBox departureDest = new TextBox(null, 400);
-        final TextBox arrivalDest = new TextBox(null, 400);
-        final TextBox dateOfDepart = new TextBox(null, 400);
-
-        final String inputString[] = new String[3];
-
-
-        addComponent(new Label("Departing From: (IATA CODE)", Terminal.Color.RED, true));
-        addComponent(departureDest);
-        addComponent(new Label("Arrival Destination", Terminal.Color.RED, true));
-        addComponent(arrivalDest);
-        addComponent(new Label("Date of Departure", Terminal.Color.RED, true));
-        addComponent(dateOfDepart);
-
-        addComponent(new Button("ENTER", new Action() {
-            @Override
-            public void doAction() {
-                inputString[0] = departureDest.getText();
-                inputString[1] = arrivalDest.getText();
-                inputString[2] = dateOfDepart.getText();
-
-                info();
-            }
-        }));
-
-
-
-
-        flc.setDepartureIATA(inputString[0]);
-        flc.setArrivalIATA(inputString[1]);
-        flc.setDateOfDeparture(inputString[2]);
-
-        addComponent(quit);
-    }
-
-    private void info() {
-
-
-        TextBox info = new TextBox("test", 400);  //formatted information from displayValues should go here
-
-        Panel panel = new Panel(new Border.Invisible(), Panel.Orientation.HORISONTAL);
-        panel.setVisible(true);
-        addComponent(panel);
-        panel.addComponent(info);
-    }
 
 
 }
